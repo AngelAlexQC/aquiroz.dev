bb32a939095307799cd4f40f68e340bad96d756a
Ángel Quiroz <angel.quiroz@edgebound.com>
2022-11-20 21:57:55 -0500

    clean home
    Signed-off-by: Ángel Quiroz <angel.quiroz@edgebound.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import Head from "next/head";
-import { trpc } from "../utils/trpc";
 import { AQuirozDev, AuthShowcase } from "../components";
 
 const Home = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -18,9 +15,6 @@ const Home = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
             <AQuirozDev />
             <AuthShowcase />
           </div>
