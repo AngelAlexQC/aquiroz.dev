090f8d9b07800713fa3c4133f041d32ee5c9d538
Ángel Quiroz <aquirozdev@gmail.com>
2025-06-15 16:44:50 -0500

    Eliminar línea en la configuración de Astro para mejorar la legibilidad

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aquiroz.dev',
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [sitemap()]
 });
