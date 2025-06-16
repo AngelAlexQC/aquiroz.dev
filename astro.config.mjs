20b3bc7dbb0dd4868751e505a64081b25fb25822
Ángel Quiroz <aquirozdev@gmail.com>
2025-06-15 20:37:33 -0500

    Agregar soporte para React en la configuración de Astro y actualizar las dependencias relacionadas

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aquiroz.dev',
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [sitemap()]
+  integrations: [sitemap(), react()]
 });
